05c650a7906828022446aba237d24ebd332b14a9
JuanPablo <bidabehere@gmail.com>
2019-03-18 14:16:21 -0300

    Version final, cambio de unidad

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
 
-const {crearArchivo, borrarArchivo } = require('./multiplicar/multiplicar');
+const {crearArchivo, listarTabla, borrarArchivo } = require('./multiplicar/multiplicar');
+ const { argv } = require ('./config/yargs')
+ const Colors = require('colors')
 
-base = process.argv[3];
-accion = process.argv[2];
+//const argv= require('yargs').argv;
 
 
-if(process.argv[2] === `crear`)
-{
-    crearArchivo(base)
+let comando = argv._[0];
+
+
+
+console.log(argv)
+
+switch (comando) {
+case 'borrar':
+ borrarArchivo(argv.base)
+ .then(data => console.log(data))
+ .catch(err => console.log(err)); 
+break;
+case 'listar':
+console.log("argv.base")
+listarTabla(argv.base,argv.limite)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+break;
+case 'crear':
+
+crearArchivo(argv.base, argv.limite)
       .then(archivo => console.log(`El archivo creado: ${archivo}`))
-     .catch(err => console.log(err));
-}else{
+      .catch(err => console.log(err));
+
+break;
+
+default:
+console.log("El comando seleccionado no corresponde")
+break;
+}
+
+
+
+
+
+
 /*
     borrarArchivo(base)
     .then(archivo => console.log(`El archivo borrado: ${archivo}`)) 
     .catch(err => console.log(err))
  //  console.log(`se borro la tabla con base ${base}`)*/
-} 
 
-console.log(process.argv[2]);
+
+
 
 
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -1,33 +1,45 @@
-
+const Colors = require('colors');
 const fs = require('fs');
 
 
-let listarTabla = (base, limite) => {
- // solo lista la tabla hasta el limite segun la base
- return new Promise (( resolve, reject) => {
-  if(!Number(base)){reject(`El valor introducido: "${base}", no es un numero`);return;}
-let data= '';
-
-for (let i=1; i <= 10; i++){ data += ` ${base} * ${i} = ${i*base} \n`; }
-resolve (data)
- })
-
-
-
-
-}
+let listarTabla = (base, limite = 10) =>
+ {
+   // solo lista la tabla hasta el limite segun la base
+  return new Promise ((resolve, reject)=>{
+    if(!Number(base)){
+      reject(`El valor introducido: "${base}", no es un numero`);
+      return;
+    }
+  if(!Number(limite)){
+    reject(`El valor introducido: "${base}", no es un numero`);
+    return;
+  }  
+  let data= '';
+  console.log(`====================`.green)
+  console.log(`Tabla del ${ base }`.blue)
+  console.log(`====================`.green)
+  for (let i=1; i <= limite; i++){
+    data += ` ${base} * ${i} = ${i*base} \n`;
+ }
+  resolve (data);
+})}
  
-let crearArchivo = (base) => {
+let crearArchivo = (base, limite = 10) => {
 
     return new Promise (( resolve, reject) => {
         if(!Number(base)){
-            reject(`El valor introducido: "${base}", no es un numero`);
+            reject(`El valor introducido como base: "${base}", no es un numero`);
             return;
         }
+        if(!Number(limite)){
+          reject(`El valor introducido como limite: "${limite}", no es un numero`);
+          return;
+      }
+
 
         let data= '';
 
-for (let i=1; i <= 10; i++){
+for (let i=1; i <= limite; i++){
    data += ` ${base} * ${i} = ${i*base} \n`;
 }
  
@@ -35,7 +47,7 @@ fs.writeFile(`./multiplicar/tabla_del_${base}.txt`, data, (err) => {
   if (err)
    reject (err);
    else 
-   resolve (`tabla_del_${base}.txt`)
+   resolve (`tabla_del_${base}.txt`.green)
   
 });
 
@@ -46,41 +58,52 @@ fs.writeFile(`./multiplicar/tabla_del_${base}.txt`, data, (err) => {
 
 } //cierro funcion crear archivo
 
-/*
-let borrarArchivo = (base) => {
-    return new Promise (( resolve, reject) => {
-        if(!Number(base)){
-            reject(`El valor introducido: "${base}", no es un numero`);
-            return;
-        }
 
-        
-        fileExists( `./multiplicar/tabla_del_${base}.txt`, (err, exists) => {
-            if(err) {
-              // manejar otro tipo de error
-            }
-            if(exists) {
-                fs.unlink(`./multiplicar/tabla_del_${base}.txt`, (err) => {
-                    if (err)
-                     reject (err);
-                     else 
-                     resolve (`el archivo tabla_del_${base}.txt fue eliminado`);
-                } else {
-                reject (`el archivo tabla_del_${base}.txt no existe`);
-            }
-          });
+let borrarArchivo = (base) => { 
+  return new Promise (( resolve, reject) => { 
+    if(!Number(base)){  reject(`El valor introducido: "${base}", no es un numero`);
+      return; }
+
+
+    fs.unlink(`./multiplicar/tabla_del_${base}.txt`, (err) => {
+      if (err) 
+      reject (err);
+      resolve (`el archivo tabla_del_${base}.txt fue eliminado`)
+    })
+
+  })//fin promesa
+
+};//fin borrarArchivo
+
+
+
+
+
+
+
+
+
+
+
+                        
+                        
+
+
+                    
+                  // resolve (`el archivo tabla_del_${base}.txt fue eliminado`);
+                    // reject (`el archivo tabla_del_${base}.txt no existe`);}
+          
 
 
 
   
-});
 
 
 
-})
 
 
-} ///*/
+
+
 
     
 
@@ -89,7 +112,8 @@ let borrarArchivo = (base) => {
 
 module.exports ={
     crearArchivo,
-    //borrarArchivo
+    listarTabla,
+    borrarArchivo
     
     }
 
